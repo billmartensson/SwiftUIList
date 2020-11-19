d69dbae07fa4e9bb967b77c77f5a918216c6181b
Bill Mårtensson <bill@magictechnology.se>
2020-11-19 09:20:02 +0100

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Bill Martensson on 2020-11-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
